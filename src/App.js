a928700210e2dc31b4a446112b6b6ec1f0a84ab5
ashjanraja <ashjannairat787@gmail.com>
2021-12-10 11:38:27 +0200

    add Footer & register page& services

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from './components/Footer';
+import Service from './components/Service';
+import Form from './components/Register';
+function App() {
+  return (
+    <div className="App">
+      <Service />
+      <Footer />
+      <Form />
+    </div>
+  );
+}
+
+export default App;
